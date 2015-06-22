fdae931cc90b7b8d78dae56811397777770cb2bd
xidianljg <xidianljg@gmail.com>
2015-06-22 22:02:46 +0800

    finish the example of ch07

---
+++ ch07/count-chars.s
@@ -0,0 +1,24 @@
+.type count_chars, @function
+.global count_chars
+.equ ST_STRING_START_ADDRESS, 8
+
+count_chars:
+pushl %ebp
+movl %esp, %ebp
+movl $0, %ecx
+movl ST_STRING_START_ADDRESS(%ebp), %edx
+
+count_loop_begin:
+movb (%edx), %al
+cmpb $0, %al
+je count_loop_end
+
+incl %ecx
+incl %edx
+
+jmp count_loop_begin
+
+count_loop_end:
+movl %ecx, %eax
+popl %ebp
+ret
